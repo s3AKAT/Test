9b9bb181615be899d760e281b104b44cb647a5db
unknown <grinner001_0@mail.ru>
2024-09-27 19:17:39 +0500

    Initialize project using Create React App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import {React, useState} from "react"
+
+function App() {
+  const [name, setName] = useState("")
+  return (
+    <>
+    inp
+    </>
+  );
+}
+
+export default App;
